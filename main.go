6bf56f535beebe6fb97f155ccb95e3e1342874d4
Fuuma <masa81hiro69@gmail.com>
2023-12-06 13:55:18 +0900

    feat: ルーム機能(まだバグがある)

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"strings"
 
@@ -12,6 +11,33 @@ import (
 // GopherInfoは、gopherの情報を表す構造体
 type GopherInfo struct {
 	ID, Name, X, Y string
+}
+
+// 各ルームごとのセッションを管理するためのマップ
+var rooms = make(map[string]map[string]map[*melody.Session]bool)
+
+// ルーム内の他のセッションにメッセージをブロードキャストするヘルパー関数
+func sendToRoom(roomID string, msg []byte) {
+	if roomSessions, ok := rooms[roomID]; ok {
+		if users, exists := roomSessions["users"]; exists {
+			for session := range users {
+				session.Write(msg)
+			}
+		}
+	}
+}
+
+// ルーム内の他のセッションにメッセージをブロードキャストするヘルパー関数（送信者自身を除く）
+func sendToRoomOthers(roomID string, msg []byte, sender *melody.Session) {
+	if roomSessions, ok := rooms[roomID]; ok {
+		if users, exists := roomSessions["users"]; exists {
+			for session := range users {
+				if session != sender {
+					session.Write(msg)
+				}
+			}
+		}
+	}
 }
 
 func main() {
@@ -30,6 +56,22 @@ func main() {
 
 	// websocketの接続時の処理
 	m.HandleConnect(func(s *melody.Session) {
+		// URLから取得したルームID（この例ではクエリパラメータとして渡す）
+		roomID := s.Request.URL.Query().Get("room")
+
+		// ルームが存在しない場合は新規作成
+		if _, ok := rooms[roomID]; !ok {
+			rooms[roomID] = make(map[string]map[*melody.Session]bool)
+		}
+
+		// ルームごとに独立したマップを作成
+		if _, ok := rooms[roomID]["users"]; !ok {
+			rooms[roomID]["users"] = make(map[*melody.Session]bool)
+		}
+
+		// ルームに参加したセッションを記録
+		rooms[roomID]["users"][s] = true
+
 		// すでに接続しているセッションの情報を取得
 		ss, _ := m.Sessions()
 
@@ -50,7 +92,7 @@ func main() {
 			s.Write([]byte("set " + info.ID + " " + info.Name + " " + info.X + " " + info.Y))
 		}
 
-		// 新しく接続したセッションに対して、idEを生成
+		// 新しく接続したセッションに対して、idを生成
 		id := uuid.NewString()
 
 		name := ""
@@ -64,6 +106,12 @@ func main() {
 
 	// websocketの切断時の処理
 	m.HandleDisconnect(func(s *melody.Session) {
+		// URLから取得したルームID（この例ではクエリパラメータとして渡す）
+		roomID := s.Request.URL.Query().Get("room")
+
+		// ルームからセッションを削除
+		delete(rooms[roomID]["users"], s)
+
 		// 切断したセッションの情報を取得
 		value, exists := s.Get("info")
 
@@ -81,26 +129,40 @@ func main() {
 
 	// websocketでメッセージを受信したときの処理
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
-		// 受信したメッセージをスペースで分割
+		roomID := s.Request.URL.Query().Get("room")
+
+		// メッセージを処理する
 		p := strings.Split(string(msg), " ")
 
-		value, _ := s.Get("info")
+		value, exists := s.Get("info")
 
-		// もし受信したメッセージが"set"で始まる場合は、
+		// nilチェックを行い、valueがnilの場合の処理を追加する
+		if !exists || value == nil {
+			// エラーハンドリングや適切な処理を行う
+			// 例えばログを出力する、デフォルト値を設定するなど
+			return
+		}
+
+		// valueがnilでない場合の処理を行う
+		info, ok := value.(*GopherInfo)
+		if !ok {
+			// インターフェース型をGopherInfoに変換できない場合のエラーハンドリングを行う
+			return
+		}
+
+		// メッセージが"set"で始まる場合の処理
 		if p[0] == "set" {
-			info := value.(*GopherInfo)
 			info.Name = p[1]
 			info.X = p[2]
 			info.Y = p[3]
-			m.BroadcastOthers([]byte("set "+info.ID+" "+info.Name+" "+info.X+" "+info.Y), s)
+			sendToRoomOthers(roomID, []byte("set "+info.ID+" "+info.Name+" "+info.X+" "+info.Y), s)
 		} else if p[0] == "chat" {
-			fmt.Println(p)
 			name := p[1]
 			text := p[2]
-			m.Broadcast([]byte("chat " + name + " " + text))
+			sendToRoom(roomID, []byte("chat "+name+" "+text))
 		}
 	})
 
 	// 5000番ポートでサーバーを起動
-	http.ListenAndServe(":5001", nil)
+	http.ListenAndServe(":5000", nil)
 }
